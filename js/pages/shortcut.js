b009056652a7c5dbe270ac6307c03e5a9e6f1674
zhouzhu85 <734837014@qq.com>
2019-05-29 17:57:25 +0800

    修改路径

--- js/pages/shortcut.js
+++ js/pages/shortcut.js
@@ -9,13 +9,13 @@ const shortcut = {
                </li>\
                <li v-else class='f-item'> \
                    请<a href='javascript:void(0)' @click='gotoLogin'>登录</a>　 \
-                   <span><a href='register.html' target='_blank'>免费注册</a></span> \
+                   <span><a href='/register.html' target='_blank'>免费注册</a></span> \
                </li> \
            </ul> \
            <ul class='fr'> \
                <li class='f-item'>我的订单</li> \
                <li class='f-item space'></li> \
-               <li class='f-item'><a href='home.html' target='_blank'>我的乐优</a></li> \
+               <li class='f-item'><a href='/home.html' target='_blank'>我的乐优</a></li> \
                <li class='f-item space'></li> \
                <li class='f-item'>乐优会员</li> \
                <li class='f-item space'></li> \
@@ -26,9 +26,9 @@ const shortcut = {
                <li class='f-item' id='service'> \
                    <span>客户服务</span> \
                    <ul class='service'> \
-                       <li><a href='cooperation.html' target='_blank'>合作招商</a></li> \
-                       <li><a href='shoplogin.html' target='_blank'>商家后台</a></li> \
-                       <li><a href='cooperation.html' target='_blank'>合作招商</a></li> \
+                       <li><a href='/cooperation.html' target='_blank'>合作招商</a></li> \
+                       <li><a href='/shoplogin.html' target='_blank'>商家后台</a></li> \
+                       <li><a href='/cooperation.html' target='_blank'>合作招商</a></li> \
                        <li><a href='#'>商家后台</a></li> \
                    </ul> \
                </li> \
@@ -52,7 +52,7 @@ const shortcut = {
     },
     methods: {
         gotoLogin() {
-            window.location = "login.html?returnUrl=" + window.location;
+            window.location = "/login.html?returnUrl=" + window.location;
         }
     }
 }
